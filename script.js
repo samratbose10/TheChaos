882bd274b2bc878e62e48056fa37c1e0726d0c77
samratbose10 <samratofficial2009@gmail.com>
2024-06-30 23:38:28 +0530

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.getElementById('button1').addEventListener('click', function() {
     this.style.display = 'none';
-    document.getElementById('button2').style.display = 'bloc';
+    document.getElementById('button2').style.display = 'block';
 });
 
 document.getElementById('button2').addEventListener('click', function() {
     this.style.display = 'none';
     document.getElementById('emoImage').style.display = 'block';
-    
+    document.getElementById('button3').style.display = 'block';
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+    document.getElementById('audio1').play();
+    document.getElementById('audio2').play();
+    document.getElementById('audio3').play();
+});
